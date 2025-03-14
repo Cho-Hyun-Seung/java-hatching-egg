75d2da8f3f59f4ea97f28d520b3c819f894c81f8
Cho-Hyun-Seung <toki0327@naver.com>
2025-03-14 23:56:20 +0900

    feat: 유저 레포지토리 필요 메소드 추가

--- src/main/java/com/toki/hatchsimulator/repository/UserRepository.java
+++ src/main/java/com/toki/hatchsimulator/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.toki.hatchsimulator.repository;
 
+import com.toki.hatchsimulator.domain.User;
+
 public interface UserRepository {
+    public User getUser();
+    public void deleteUser(User user);
+    public void addUser(User user);
+
 }
--- src/main/java/com/toki/hatchsimulator/repository/UserRepositoryImpl.java
+++ src/main/java/com/toki/hatchsimulator/repository/UserRepositoryImpl.java
@@ -1,4 +1,21 @@
 package com.toki.hatchsimulator.repository;
 
+import com.toki.hatchsimulator.domain.User;
+
 public class UserRepositoryImpl implements UserRepository{
+
+    @Override
+    public User getUser() {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(User user) {
+
+    }
+
+    @Override
+    public void addUser(User user) {
+
+    }
 }
